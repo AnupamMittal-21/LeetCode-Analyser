057128374a176b60fd689b5896114e87a54088bf
AnupamMittal-21 <anupammittal1234@gmail.com>
2023-08-25 13:33:01 +0530

    App was crashing at contest questions, so I fixed it by manipulating link of the submission

--- analysis/constants.py
+++ analysis/constants.py
@@ -2,5 +2,5 @@ PATH = r"C:\ChromeDriver\chromedriver.exe"
 DEFAULT_BROWSER = r'--user-data-dir=C:\Users\anupa\AppData\Local\Google\Chrome\User Data\Default'
 BASE_URL = "https://leetcode.com/accounts/login/"
 
-# username = "demo_account_12"
-# password = "demo_account_12"
+username = "demo_account_12"
+password = "demo_account_12"
